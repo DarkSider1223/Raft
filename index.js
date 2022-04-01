c8da0e05121504877411059363c6448d757f21fd
DarkSider1223 <moahmedirfansy@gmail.com>
2022-04-01 22:46:47 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js"); //discord.js v13
+const config = require(`./config.json`);
+
+const Enmap = require("enmap");
+const client = new Discord.Client({
+    shards: "auto",
+    failIfNotExists: false, //add this for security!
+    allowedMentions: { parse: [ ], repliedUser: false },
+    intents: [ 
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+    ],
+});
+client.settings = new Enmap({
+    name: "NumberCounter",
+});
+
+client.login(config.token)
+
+client.on("ready", () => {
+    console.log(`Logged in ${client.user.tag}`)
+    //import our number counter!
+    require("./numcounter")(client); // we pass in the client!
+})
